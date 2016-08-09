81807acdee3ef3eea2b835983c515bf370db0c0e
jzayac <zayac123@gmail.com>
2016-08-10 01:31:48 +0200

    socket update

---
+++ api/socket/index.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const user = {};
+const rooms = [];
+
+module.exports = function(io) {
+  io.on('connection', (socket) => {
+    // console.log("Query: ", socket.handshake.data);
+    socket.join('Test');
+
+    socket.on('question', (channelID) => {
+      socket.broadcast.to(channelID).emit('new bc message', msg);
+    });
+
+    socket.on('authorization', (userData) => {
+      user[userData.name] = userData;
+    });
+
+    socket.on('set store', (data) => {
+      user.data = data;
+    });
+    socket.on('get store', () => {
+      console.log(user);
+      socket.emit('data', user);
+    });
+
+    socket.on('join channel', (channel) => {
+      socket.join(channel.name);
+    });
+
+    // socket.emit('news', {msg: `'Hello World!' from server`});
+
+    socket.on('history', () => {
+      for (let index = 0; index < bufferSize; index++) {
+        const msgNo = (messageIndex + index) % bufferSize;
+        const msg = messageBuffer[msgNo];
+        if (msg) {
+          socket.emit('msg', msg);
+        }
+      }
+    });
+
+    socket.on('msg', (data) => {
+      data.id = messageIndex;
+      messageBuffer[messageIndex % bufferSize] = data;
+      messageIndex++;
+      io.emit('msg', data);
+    });
+    socket.on('disconnect', function() {
+      delete user[socket.id];
+    });
+  });
+}
---
+++ app/socket/mainEvents.js
@@ -0,0 +1,20 @@
+export default (io) => {
+  io.on('news', (data) => {
+    console.log(data);
+    io.emit('my other event', { my: 'data from client' });
+  });
+
+  io.on('msg', (data) => {
+    console.log(data);
+  });
+
+  io.on('error', (error) => {
+    console.error('SOCKET error :' + error);
+  });
+
+  // io.on('join channel', (channel) => {
+  //   io.join(channel.name);
+  // });
+
+  io.emit('user/login', { user: 'name' });
+};
